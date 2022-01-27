abfa7ec33c314d11a02f6660c89c7239279cd34c
thelovemsg <thelovemsg@naver.com>
2022-01-27 23:17:32 +0900

    update study

---
+++ mvc2/login/src/main/java/hello/login/web/member/Member.java
@@ -0,0 +1,19 @@
+package hello.login.web.member;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Member {
+
+    private Long id;
+
+    @NotEmpty
+    private String loginId; // 로그인 ID
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String password;
+
+}
