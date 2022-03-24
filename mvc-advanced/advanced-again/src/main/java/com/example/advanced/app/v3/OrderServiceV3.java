a8829a30054d029532ebdf97f378a626d7fb8797
unknown <thelovemsg@naver.com>
2022-03-25 01:01:38 +0900

    updat sutry

---
+++ mvc-advanced/advanced-again/src/main/java/com/example/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,38 @@
+package com.example.advanced.app.v3;
+
+import com.example.advanced.trace.TraceStatus;
+import com.example.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    /*public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(traceId, "OrderServiceV2.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }*/
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderServiceV2.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
---
+++ mvc-advanced/advanced-again/src/test/java/com/example/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.advanced.trace.threadlocal;
+
+import com.example.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field(){
+        log.info("main start");
+        Runnable userA = () -> {;
+            service.logic("userA");
+        };
+
+        Runnable userB = () -> {;
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);//동시성 문제가 발생하지 않는 코드
+        sleep(100);//일부러 동시성 문제 발생
+        threadB.start();
+        
+        sleep(3000);//메인 쓰레드 종료 대기
+        log.info("main exit");
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
