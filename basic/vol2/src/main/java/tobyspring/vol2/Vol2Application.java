fc8d51910c2c53e369fdd84b351d3dea72db1325
thelovemsg <thelovemsg@naver.com>
2022-12-25 17:32:08 +0900

    토비의 스프링 vol2 공부중...

---
+++ basic/vol2/src/main/java/tobyspring/vol2/Vol2Application.java
@@ -0,0 +1,13 @@
+package tobyspring.vol2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vol2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vol2Application.class, args);
+	}
+
+}
